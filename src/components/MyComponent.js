809f8215f6c5106f9489a3025fa4bd4e33aa366d
Diep Quoc Phuc <dqphuc29@gmail.com>
2022-08-30 22:37:20 +0700

    video 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,16 @@
+// class component
+//function component
+
+import React from "react";
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        {" "}
+        my first component
+        {Math.random()}
+      </div>
+    );
+  }
+}
+export default MyComponent;
