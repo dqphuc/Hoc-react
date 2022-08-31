62abe815307b399a816f13faaf9ea1878f0336fe
Diep Quoc Phuc <dqphuc29@gmail.com>
2022-08-31 21:00:20 +0700

    video 17

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,12 +3,17 @@
 
 import React from "react";
 class MyComponent extends React.Component {
+  state = {
+    name: "Phuc",
+    address: "Kien Giang",
+    age: 40,
+  };
+
   render() {
     return (
       <div>
-        {" "}
-        my first component
-        {Math.random()}
+        My name is {this.state.name} I'm from {this.state.address} I'm{" "}
+        {this.state.age} year old
       </div>
     );
   }
