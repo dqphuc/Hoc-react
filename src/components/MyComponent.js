c5ce4c977b680f18510e55b4b508a49577a10aeb
Diep Quoc Phuc <dqphuc29@gmail.com>
2022-08-31 21:40:42 +0700

    video 19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,12 +8,17 @@ class MyComponent extends React.Component {
     address: "Kien Giang",
     age: 40,
   };
+  handleClick(event) {
+    //console.log(">>>>> click me my button");
+    console.log(event.target)
+  }
 
   render() {
     return (
       <div>
         My name is {this.state.name} I'm from {this.state.address} I'm{" "}
         {this.state.age} year old
+        <button onClick={this.handleClick}>Click me</button>
       </div>
     );
   }
