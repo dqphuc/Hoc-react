8ec03ff1b406a4a83bc5f976b530edfcfa0c92b2
Diep Quoc Phuc <dqphuc29@gmail.com>
2022-09-01 15:40:52 +0700

    video 20

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -9,8 +9,15 @@ class MyComponent extends React.Component {
     age: 40,
   };
   handleClick(event) {
-    //console.log(">>>>> click me my button");
-    console.log(event.target)
+    console.log(">>>>> click me my button");
+    
+    this.setState({
+      name: "Diep Quoc Phuc",
+      age: Math.floor((Math.random()*100)+1)
+    });
+  }
+  handleOnMouseOver(event) {
+    console.log(event.pageX);
   }
 
   render() {
@@ -18,7 +25,14 @@ class MyComponent extends React.Component {
       <div>
         My name is {this.state.name} I'm from {this.state.address} I'm{" "}
         {this.state.age} year old
-        <button onClick={this.handleClick}>Click me</button>
+        <button onMouseOver={this.handleOnMouseOver}>Hoverme</button>
+        <button
+          onClick={(event) => {
+            this.handleClick(event);
+          }}
+        >
+          Click me
+        </button>
       </div>
     );
   }
