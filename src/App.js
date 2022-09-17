26fd00a4271952ad569bc59c8804e7e12460d68c
Diep Quoc Phuc <dqphuc29@gmail.com>
2022-09-17 20:45:24 +0700

    video 49

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import "./App.scss";
 import Header from "./components/Header/header";
-import { Link } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+
 const App = () => {
   return (
     <div className="app-container">
-      <Header />
-      <div>
-        test link
-        <div>
-          <button>
-            <Link to="/users">go to User page</Link>
-          </button>
-          <button>
-            <Link to="/admins">go to Admin page</Link>
-          </button>
-        </div>
+      <div className="header-container">
+        <Header />
       </div>
+      <div className="main-container">
+        <div className="sidenav-container"></div>
+        <div className="app-content"></div>
+        <Outlet />
+      </div>
+      
     </div>
   );
 };
