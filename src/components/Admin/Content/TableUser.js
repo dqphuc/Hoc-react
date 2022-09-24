6517dbb45df5bdec6505f46e07f8b1940167fc22
Diep Quoc Phuc <dqphuc29@gmail.com>
2022-09-24 22:26:35 +0700

    video 62

--- src/components/Admin/Content/TableUser.js
+++ src/components/Admin/Content/TableUser.js
@@ -1,26 +1,14 @@
-import { useEffect, useState } from "react";
-import {getAllUsers} from "../../../services/apiService";
+
 
 const TableUser = (props) =>{
-    const [listUsers, setListUsers] = useState([])
+    const {listUsers} = props
 
-    useEffect(()=>{
-        fetchListUsers ();
-    }, [])
-    const fetchListUsers = async ()=>{
-       let res = await getAllUsers()
-       if (res.EC ===0){
-        setListUsers(res.DT);
-       }
-    }
-    console.log ('render view')
-    
-    return (
+       return (
         <>
         <table className ="table table-hover table-bordered">
         <thead>
             <tr>
-            <th scope="col">No</th>
+            <th scope="col">ID</th>
             <th scope="col">Username</th>
             <th scope="col">Email</th>
             <th scope="col">Role</th>
@@ -32,7 +20,7 @@ const TableUser = (props) =>{
           listUsers.map((item, index)=>{
             return(
                 <tr key={'table-users - ${index}'}>
-                    <td>{index+1}</td>
+                    <td>{item.id}</td>
                     <td>{item.username}</td>
                     <td>{item.email}</td>
                     <td>{item.role}</td>
